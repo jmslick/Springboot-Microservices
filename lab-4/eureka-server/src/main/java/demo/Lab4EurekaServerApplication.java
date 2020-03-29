2035680e459a6fda89a75dbb73cb5201e91a856f
unknown <jmslick@gmail.com>
2020-03-28 21:46:29 -0500

    ir

---
+++ lab-4/eureka-server/src/main/java/demo/Lab4EurekaServerApplication.java
@@ -0,0 +1,15 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class Lab4EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab4EurekaServerApplication.class, args);
+	}
+
+}
